4e4fe04111e8d453b5a391cf8b45c84ab1bd0c9a
Martin <mutugimartin967@gmail.com>
2024-10-19 19:48:35 +0300

    Added start script and cleaned

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceKeyAccounts.json');  // Ensure this file exists and contains valid credentials
 
-// Initialize the Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://netdokproject-default-rtdb.firebaseio.com"  // Make sure this URL is correct
-});
+// Check if Firebase Admin has already been initialized to prevent duplicate initialization
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://netdokproject-default-rtdb.firebaseio.com"  // Ensure this URL is correct
+  });
+}
 
 // Get the Firebase Realtime Database instance
 const db = admin.database();
